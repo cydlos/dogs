0619d730565fea3e7dbb571e059882f8daef43ce
cyd losekann <cydnel@gmail.com>
2022-10-30 07:49:46 -0300

    inputs

--- src/api/endpoints/UserPost.js
+++ src/api/endpoints/UserPost.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
 const UserPost = () => {
-  const [user, setUser] = React.useState('');
+  const [username, setUsername] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
 
   return (
-    <h2>User Post</h2>
+    <div>
+      <input
+        type="text"
+        value={username}
+        onChange= {({target})=> setUsername(target.value)}
+        />
+
+      <input
+        type="text"
+        value={email}
+        onChange= {({target}) => setEmail(target.value)}
+        />
+
+      <input
+        type="text"
+        value={password}
+        onChange= {({target}) => setPassword(target.value)}
+        />
+    </div>
   )
 }
 
