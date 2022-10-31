0735469c9b6ad54c91d7e2e84278b2101852969f
cyd losekann <cydnel@gmail.com>
2022-10-31 06:30:51 -0300

    path fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Home from "./Home";
+import Home from  "./Components/Home" ;
 import Login from "./Components/Login/Login";
 
 function App() {
