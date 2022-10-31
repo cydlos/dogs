8b218fcba52aaa615d0ea98a4e47fa9fe37dff43
cyd losekann <cydnel@gmail.com>
2022-10-31 06:28:05 -0300

    format

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Home';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Home";
+import Login from "./Components/Login/Login";
 
 function App() {
-  return <div>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-    </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import styles from './Header.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Header = () => {
-  return <div className={styles.header}> Header
-    <nav>
-      <Link to ="/">Home</Link>
-      <Link to ="/login">Login / Criar</Link>
-    </nav>
-  </div>;
+  return (
+    <div className={styles.header}>
+      {" "}
+      Header
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Criar</Link>
+      </nav>
+    </div>
+  );
 };
 
-export default Header
+export default Header;
