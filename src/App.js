771568a7fb43ff7dcd5a07e604e20752652d4b6c
cyd losekann <cydnel@gmail.com>
2022-10-31 06:22:05 -0300

    links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import { BrowserRouter } from 'react-router-dom';
+import Home from './Home';
+
 
 function App() {
   return <div>
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
     </div>;
