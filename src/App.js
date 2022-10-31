b6f3e7305d713876f2d5e8514e1f4eaef2f891a9
cyd losekann <cydnel@gmail.com>
2022-10-31 06:10:52 -0300

    browse router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import Api from './api/Api';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return (
-    <div> <Api /></div>
-  );
+  return <div>
+    <BrowserRouter>
+      <Header />
+      <Footer />
+    </BrowserRouter>
+    </div>;
 }
 
 export default App;
