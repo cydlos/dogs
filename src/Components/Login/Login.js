f6353fb21f7d0e82c1b1ce1d768fdc7faacb3adb
cyd losekann <cydnel@gmail.com>
2022-11-04 20:18:54 -0300

    login password reset

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LoginForm from './LoginForm'
 import LoginCreate from './LoginCreate'
-import LoginPasswordLost from './LoginPasswordLost' 
+import LoginPasswordLost from './LoginPasswordLost'
+import LoginPasswordReset from './LoginPasswordReset'
+
 const Login = () => {
   return  <div>
     <Routes>
