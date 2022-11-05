ffbe6caf2d05f131dc65b6df97481f313a006d50
cyd losekann <cydnel@gmail.com>
2022-11-05 01:45:06 -0300

    form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(username, password);
+  }
 
   return (
     <section>
       <h1>Login</h1>
-      <form action>
+      <form action="" onSubmit={handleSubmit}>
         <input
-        type="text"
-        value={username}
-        onChange={({target}) => setUsername(target.value)} />
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
