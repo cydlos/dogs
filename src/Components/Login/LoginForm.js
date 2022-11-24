b4318580a143fc143561e1a8a9bb35f37e2d86e6
cyd losekann <cydnel@gmail.com>
2022-11-24 03:46:13 -0300

    className fixed

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -40,7 +40,7 @@ const LoginForm = () => {
       <div className={styles.cadastro}>
         <h2 className={styles.subtitle}>Cadastre-se</h2>
         <p>Ainda não possui conta? Cadastre-se no site.</p>
-        <Link classname={stylesBtn.button} to="/login/criar">
+        <Link className={stylesBtn.button} to="/login/criar">
           Cadastro
         </Link>
       </div>
