e42ceb51ab250d863823ca170bc6e8e7c482ce48
cyd losekann <cydnel@gmail.com>
2022-11-26 01:23:03 -0300

    user post

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -3,21 +3,29 @@ import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
 
-
 const LoginCreate = () => {
   const username = useForm();
   const email = useForm("email");
   const password = useForm("password");
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+    const url = "https://dogsapi.origamid.dev/json/api/user";
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response =  await fetch()
   }
 
   return (
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Cadastrar</Button>
       </form>
     </section>
