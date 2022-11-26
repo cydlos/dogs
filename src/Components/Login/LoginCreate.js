f451d011f5ed8774162e444b6fcd451ee4be70cb
cyd losekann <cydnel@gmail.com>
2022-11-25 23:13:43 -0300

    form

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const LoginCreate = () => {
-  return <section className="animeLeft">
-    <h1>Cadastre-se</h1>
-  </section>;
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Usuário" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <Link>Cadastrar</Link>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
