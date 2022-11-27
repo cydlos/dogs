bdf6bc0a8d9880c57904cf510d876c6e3dcdc77d
cyd losekann <cydnel@gmail.com>
2022-11-27 18:59:59 -0300

    navigate added

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { UserContext } from '../../UserContext'
+import { Navigate } from 'react-router-dom'
 
 const ProtectedRoute = () => {
   const {login} = React.useContext(UserContext)
