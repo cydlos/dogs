2b29d6fe928b40ca9556c95310fb5018b0677c09
cyd losekann <cydnel@gmail.com>
2022-11-26 23:04:26 -0300

    user

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
+            <Route path="user/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
