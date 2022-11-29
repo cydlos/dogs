b75f004de308da13a4003400ff784a69f6fda140
cyd losekann <cydnel@gmail.com>
2022-11-29 01:23:54 -0300

    header

--- src/User/User.js
+++ src/User/User.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import UserHeader from "./UserHeader";
 import Feed from "./Feed";
 import UserPhotoPost from "./UserPhotoPost";
+import UserStats from "./UserStats";
 
 const User = () => {
   return <section className="container">
