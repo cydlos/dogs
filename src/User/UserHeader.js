375953d652545a0f5affb660f3dfdb6d6d26d8a6
cyd losekann <cydnel@gmail.com>
2022-11-30 18:57:24 -0300

    user header nav

--- src/User/UserHeader.js
+++ src/User/UserHeader.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import UserHeaderNav from './UserHeaderNav'
 
 const UserHeader = () => {
   return (
     <header>
       <h1 className="title">Título</h1>
+      <UserHeaderNav />
     </header>
   )
 }
---
+++ src/User/UserHeaderNav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserHeaderNav = () => {
+  return (
+    <div>UserHeaderNav</div>
+  )
+}
+
+export default UserHeaderNav
