d9310828fc9dacfb57c80b1a15d4480ce491f3a2
cyd losekann <cydnel@gmail.com>
2022-11-30 19:17:57 -0300

    user header nav

--- src/User/UserHeaderNav.js
+++ src/User/UserHeaderNav.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../../UserContext";
+import { ReactComponent as MinhasFotos } from "../../Assets/feed.svg";
+import { ReactComponent as Estatisticas } from "../../Assets/estatisticas.svg";
+import { ReactComponent as AdicionarFoto } from "../../Assets/adicionar.svg";
+import { ReactComponent as Estatisticas } from "../../Assets/estatisticas.svg";
+import { ReactComponent as AdicionarFoto } from "../../Assets/adicionar.svg";
 
 const UserHeaderNav = () => {
-  const {UserLogout} = React.useContext(UserContext);
+  const { UserLogout } = React.useContext(UserContext);
 
   return (
     <nav>
-      <NavLink to="/conta">Minhas Fotos </NavLink>
-      <NavLink to="/conta/estatisticas"> Estatísticas </NavLink>
+      <NavLink to="/conta">
+        <MinhasFotos />
+        Minhas Fotos
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        Estatísticas{" "}
+      </NavLink>
       <NavLink to="/consta/postar"> Adicionar Foto </NavLink>
       <button onClick={UserLogout}> Sair </button>
     </nav>
