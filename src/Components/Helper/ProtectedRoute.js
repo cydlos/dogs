39ce7759cf000472429dc50983367139ebca914e
cyd losekann <cydnel@gmail.com>
2022-11-30 02:42:30 -0300

    comments

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -5,6 +5,9 @@ import { Navigate } from 'react-router-dom'
 const ProtectedRoute = ({children}) => {
   const {login} = React.useContext(UserContext)
   return  login ? children : <Navigate to="/login" />;
+  // verifica se o usuário está logado;
+  // se estiver, envia o usuário para a página User, área protegida
+  // se não estiver, envia o usuário para a página de login;
 
 }
 
