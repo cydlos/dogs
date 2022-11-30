7cb86ea241ed029fb327791ab839b27b26c06fb6
cyd losekann <cydnel@gmail.com>
2022-11-30 01:58:00 -0300

    correct route

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
             <Route
-              path="user/*"
+              path="conta/*"
               element={
                 <ProtectedRoute>
                   {" "}
