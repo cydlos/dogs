93e4b04295157e26d1f44f1778fc10834a1f27ed
cyd losekann <cydnel@gmail.com>
2022-11-30 19:03:55 -0300

    button sair added

--- src/User/UserHeaderNav.js
+++ src/User/UserHeaderNav.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 
 const UserHeaderNav = () => {
+  const {UserLogout} = React.useContext(UserContext);
+
   return (
     <nav>
       <NavLink to="/conta">Minhas Fotos </NavLink>
       <NavLink to="/conta/estatisticas"> Estatísticas </NavLink>
       <NavLink to="/consta/postar"> Adicionar Foto </NavLink>
-      <button> Sair </button>
+      <button onClick={UserLogout}> Sair </button>
     </nav>
   );
 };
