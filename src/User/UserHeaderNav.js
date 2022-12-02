6d5867de3e503a5855641f0a78e3a7b9fcb4007e
cyd losekann <cydnel@gmail.com>
2022-12-02 00:28:22 -0300

    sair

--- src/User/UserHeaderNav.js
+++ src/User/UserHeaderNav.js
@@ -4,10 +4,10 @@ import { UserContext } from "../../UserContext";
 import { ReactComponent as MinhasFotos } from "../../Assets/feed.svg";
 import { ReactComponent as Estatisticas } from "../../Assets/estatisticas.svg";
 import { ReactComponent as AdicionarFoto } from "../../Assets/adicionar.svg";
-import { ReactComponent as AdicionarFoto } from "../../Assets/adicionar.svg";
+import { ReactComponent as Sair } from "../../Assets/sair.svg";
 
 const UserHeaderNav = () => {
-  const { UserLogout } = React.useContext(UserContext);
+  const { userLogout } = React.useContext(UserContext);
 
   return (
     <nav>
@@ -24,7 +24,7 @@ const UserHeaderNav = () => {
         <AdicionarFoto />
         Adicionar Foto{" "}
       </NavLink>
-      <button onClick={UserLogout}> Sair </button>
+      <button onClick={userLogout}>  <Sair />Sair</button>
     </nav>
   );
 };
