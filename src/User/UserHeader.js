06dd31cdb6baef7e29cac497377aa8022a6d39bf
cyd losekann <cydnel@gmail.com>
2022-12-06 00:43:10 -0300

    user header

--- src/User/UserHeader.js
+++ src/User/UserHeader.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import UserHeaderNav from './UserHeaderNav'
-import styles from './UserHeader.module.css'
+import React from "react";
+import UserHeaderNav from "./UserHeaderNav";
+import styles from "./UserHeader.module.css";
 
 const UserHeader = () => {
+  const [title, setTitle] = React.useState("");
+  const location = React.useLocation();
+
+  React.useEffect(() => {
+    setTitle(location.pathname);
+    if (location.pathname === "/conta") {
+      setTitle("Minha conta");
+    }
+  }, [location]);
+
   return (
     <header className={styles.header}>
-      <h1 className="title">Minha Conta</h1>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
-  )
-}
+  );
+};
 
-export default UserHeader
+export default UserHeader;
