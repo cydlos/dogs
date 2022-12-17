496c8d3065ff65f44145eddd3e3b4a7bfe35721b
cyd losekann <cydnel@gmail.com>
2022-12-17 01:24:56 -0300

    number checking

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -9,7 +9,7 @@ const types = {
     regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
     message: 'A senha precisa ter 1 caracter alfanumérico, 1 caracter especial e no mínimo 8 caracteres.',
   },
-
+number:  { }
 };
 
 const useForm = (type) => {
