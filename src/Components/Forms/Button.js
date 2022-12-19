9cbe1cc2d6d6bff6a9136ac8efd932e7a5738a7f
cyd losekann <cydnel@gmail.com>
2022-12-19 00:31:09 -0300

    comment on form data

--- src/Components/Forms/Button.js
+++ src/Components/Forms/Button.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import styles from './Button.module.css'
+import React from "react";
+import styles from "./Button.module.css";
 
-const Buttons = ({children, ...props}) => {
+const Buttons = ({ children, ...props }) => {
   return (
-    <button {...props} className={styles.button}>{children}</button>
-  )
-}
+    <button {...props} className={styles.button}>
+      {children}
+    </button>
+  );
+};
 
-export default Buttons
+export default Buttons;
