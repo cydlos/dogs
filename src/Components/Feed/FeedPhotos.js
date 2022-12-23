cde5d34965d48864a560b6bbe6d793c61bcaf38c
cyd losekann <cydnel@gmail.com>
2022-12-23 16:24:27 -0300

    if error etc

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FeedPhotosItem from "./FeedPhotosItem";
 import useFetch from "../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../api";
 
 const FeedPhotos = () => {
 
@@ -10,10 +11,14 @@ const FeedPhotos = () => {
     async function fetchPhotos() {
       const {url, options} = PHOTOS_GET({page: 1, total: 6, user: 0});
       // user 0 porque não vai puxar de nenhum usuário específico.
-      request (url, options) }
+      const {response, json} = await request (url, options) }
     fetchPhotos();
   }, [request]);
-  
+
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data) return <p>Foto carregada!</p>;
+
 
   return (
     <div>
