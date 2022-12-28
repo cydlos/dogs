debd586f23346dbe1d7209c1ad30bde7f8201dc8
cyd losekann <cydnel@gmail.com>
2022-12-28 01:40:32 -0300

    feed modal

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./FeedModal.module.css";
+import useFetch from "../../Hooks/useFetch";
 
 const FeedModal = ({ photo }) => {
   const { data, loading, error, request } = useFetch();
 
-  return <div className={styles.modal}></div>;
+  React.useEffect(() => {
+    const { url, options } = PHOTOS_GET({ id: photo.id });
+    request(url, options);
+  }, [photo, request]);
+
+  return (
+  <div className={styles.modal}></div>
+  );
 };
 
 export default FeedModal;
