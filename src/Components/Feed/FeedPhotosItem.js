84b18546db4770f51e60e70cab618841f60666da
cyd losekann <cydnel@gmail.com>
2022-12-28 01:16:51 -0300

    visualizacao

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -4,7 +4,7 @@ import styles from './FeedPhotosItem.module.css'
 const FeedPhotosItem = ({photo} ) => {
   return <li className={styles.photo}>
     <img src={photo.src} alt={photo.title} />
-    <span>{photo.acessos}</span>
+    <span className={styles.visualizacao}>{photo.acessos}</span>
   </li>
 }
 
