4cd717b2db7bffa3343372736b6b750de2e16357
cyd losekann <cydnel@gmail.com>
2023-01-07 01:13:25 -0300

    userphotopost

--- src/User/UserPhotoPost.js
+++ src/User/UserPhotoPost.js
@@ -1,37 +1,35 @@
-import React from "react";
-import styles from "./UserPhotoPost.module.css";
-import Input from "../Components/Forms/Input";
-import useForm from "../Hooks/useForm";
-import Button from "../Components/Forms/Button";
-import useFetch from "../Hooks/useFetch";
-import Error from "../Components/Helper/Error";
-import { PHOTO_POST } from "../api";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import styles from './UserPhotoPost.module.css';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import Error from '../Helper/Error';
+import { PHOTO_POST } from '../../Api';
+import { useNavigate } from 'react-router-dom';
+import Head from '../Helper/Head';
 
 const UserPhotoPost = () => {
   const nome = useForm();
-  const peso = useForm("number");
-  const idade = useForm("number");
-
+  const peso = useForm('number');
+  const idade = useForm('number');
   const [img, setImg] = React.useState({});
   const { data, error, loading, request } = useFetch();
-  const navigate = useNavigate;
+  const navigate = useNavigate();
+
   React.useEffect(() => {
-    if (data) navigate("/conta");
+    if (data) navigate('/conta');
   }, [data, navigate]);
 
   function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData();
-    formData.append("img", img.raw);
-    formData.append("nome", nome.value);
-    formData.append("peso", peso.value);
-    formData.append("idade", idade.value);
+    formData.append('img', img.raw);
+    formData.append('nome', nome.value);
+    formData.append('peso', peso.value);
+    formData.append('idade', idade.value);
 
-    // não pode ser um json pois inclui um arquivo de imagem, por isso utiliza-se Form Data
-
-    const token = window.localStorage.getItem("token");
-
+    const token = window.localStorage.getItem('token');
     const { url, options } = PHOTO_POST(formData, token);
     request(url, options);
   }
@@ -44,31 +42,27 @@ const UserPhotoPost = () => {
   }
 
   return (
-    <section className={` ${styles.photoPost} animeLeft`}>
+    <section className={`${styles.photoPost} animeLeft`}>
+      <Head title="Poste sua foto" />
       <form onSubmit={handleSubmit}>
         <Input label="Nome" type="text" name="nome" {...nome} />
         <Input label="Peso" type="number" name="peso" {...peso} />
         <Input label="Idade" type="number" name="idade" {...idade} />
-        <Input
+        <input
           className={styles.file}
           type="file"
           name="img"
           id="img"
           onChange={handleImgChange}
         />
-                {loading ? (
+        {loading ? (
           <Button disabled>Enviando...</Button>
         ) : (
           <Button>Enviar</Button>
         )}
+        <Error error={error} />
       </form>
       <div>
-        <Error error={error} />
-        {data && (
-          <p>
-            <img src={data.src} alt={data.nome} />
-          </p>
-        )}
         {img.preview && (
           <div
             className={styles.preview}
