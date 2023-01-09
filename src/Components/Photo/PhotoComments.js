c036335fd4e7aea4a319c48fb5fd4183419dfb20
cyd losekann <cydnel@gmail.com>
2023-01-09 06:10:18 -0300

    photo comments

---
+++ src/Components/Photo/PhotoComments.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./photoComments.module.css";
+
+
+const photoCommentst = ({ comments }) => (
+  <div>
+    {comments.map((comment) => (
+      <div key={comment.id} className="comment">
+        <div className={styles.user}>{comment.user}</div>
+        <div className={styles.text}>{comment.text}</div>
+      </div>
+    ))}
+  </div>
+);
+
+export default photoCommentst;
