649079b7a1591a09538baf103a69763843d47f45
cyd losekann <cydnel@gmail.com>
2023-01-16 01:45:49 -0300

    photo comments

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import {UserContext} from '../../UserContext';
+import React from "react";
+import { UserContext } from "../../UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
 
-const PhotoComments = () => {
-  const {login} = React.useContext(UserContext);
-  return <div>
-    PhotoComments
-    </div>;
+const PhotoComments = (props) => {
+  const { login } = React.useContext(UserContext);
+  return <div>{login && <PhotoCommentsForm id={props.id} />}</div>;
 };
 
 export default PhotoComments;
