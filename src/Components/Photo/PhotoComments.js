5989bca1f90ecb041a4e4bb7766992327b7f1251
cyd losekann <cydnel@gmail.com>
2023-01-16 18:18:15 -0300

    comentários e autor

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -3,8 +3,20 @@ import { UserContext } from "../../UserContext";
 import PhotoCommentsForm from "./PhotoCommentsForm";
 
 const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
   const { login } = React.useContext(UserContext);
-  return <div>{login && <PhotoCommentsForm id={props.id} />}</div>;
+  return (
+    <div>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author} </b>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} />}
+    </div>
+  );
 };
 
 export default PhotoComments;
