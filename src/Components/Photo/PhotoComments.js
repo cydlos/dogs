aa7b516a86bc061eee412d2a19d1a357bb7fe2d9
cyd losekann <cydnel@gmail.com>
2023-01-16 18:20:42 -0300

    photo comments css

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { UserContext } from "../../UserContext";
 import PhotoCommentsForm from "./PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
 
 const PhotoComments = (props) => {
   const [comments, setComments] = React.useState(() => props.comments);
   const { login } = React.useContext(UserContext);
   return (
     <div>
-      <ul>
+      <ul className= {styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
-            <b>{comment.comment_author} </b>
+            <b>{comment.comment_author}:  </b>
+            <span>{comment.comment_content}  </span>
           </li>
         ))}
       </ul>
