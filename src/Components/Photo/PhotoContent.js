dfc5a2592d9e6db4c829e6192eba0423ce9106e9
cyd losekann <cydnel@gmail.com>
2023-01-27 21:39:59 -0300

    user context added to photo content

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./PhotoContent.module.css";
 import PhotoComments from "./PhotoComments";
+import PhotoDelete from "./PhotoDelete";
+import { UserContext } from "../../UserContext";
 
 const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
+  const user = React.useContext(UserContext);
 
   return (
     <div className={styles.photo}>
@@ -13,6 +16,11 @@ const PhotoContent = ({ data }) => {
         <div className={styles.details}>
           <div>
             <p className={styles.author}>
+              {user.data && user.data.username === photo.author ? (
+                <PhotoDelete id={photo.id} />
+              ) : (
+                <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+              )}
               <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
               <span className={styles.visualizacoes}>{photo.acessos}</span>
             </p>
