661f05b8d4f932cb6ef885b46a739067cbf20762
cyd losekann <cydnel@gmail.com>
2023-01-27 21:34:38 -0300

    photo delete

---
+++ src/Components/Photo/PhotoDelete.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./PhotoDelete.module.css";
+
+const PhotoDelete = ({ id }) => {
+  return <button>Deletar</button>;
+};
+
+export default PhotoDelete;
