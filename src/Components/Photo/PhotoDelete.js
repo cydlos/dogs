951d5f2c9fc01b593f8b4e85f14d52247c3a5162
cyd losekann <cydnel@gmail.com>
2023-01-27 21:58:25 -0300

    photo delete

--- src/Components/Photo/PhotoDelete.js
+++ src/Components/Photo/PhotoDelete.js
@@ -1,12 +1,23 @@
 import React from "react";
 import styles from "./PhotoDelete.module.css";
+import { PHOTO_DELETE } from "../../api";
 
 const PhotoDelete = ({ id }) => {
-  
+  const { loading, request } = useFetch();
+  async function handleClick(event) {
+    event.preventDefault();
+    const { url, options } = PHOTO_DELETE(id);
+    const { response } = await request(url, options);
+  }
+
   return (
     <div>
-    <button className={styles.delete}>Deletar</button>;
+      <button onClick={handleClick} className={styles.delete}>
+        Deletar
+      </button>
+      ;
     </div>
-)};
+  );
+};
 
 export default PhotoDelete;
