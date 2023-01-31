840f7eee47062ce0e714150738266db58170529b
cyd losekann <cydnel@gmail.com>
2023-01-30 22:14:22 -0300

    img skeleton

--- src/Components/Helper/Image.js
+++ src/Components/Helper/Image.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import styles from './Image.module.css'
+import React from "react";
+import styles from "./Image.module.css";
 
 const Image = () => {
-  return (
-    <div>Image</div>
-  )
-}
+  return <div className={styles.wrapper}>
+    <div className={styles.skeleton}></div>
+    <img className={styles.img} src="" alt="" />
+  </div>;
+};
 
-export default Image
+export default Image;
