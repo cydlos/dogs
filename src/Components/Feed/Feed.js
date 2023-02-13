0f868202a58458d727fcdc11ec1cc72222782917
cyd losekann <cydnel@gmail.com>
2023-02-12 22:27:45 -0300

    infinite scroll

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import FeedModal from './FeedModal'
-import FeedPhotos from './FeedPhotos'
+import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhotos from "./FeedPhotos";
 
-const Feed = ({user}) => {
+const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
 
   React.useEffect(() => {
-    function  infiniteScroll() {
-
+    function infiniteScroll() {
+      if (
+        window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight
+      )
+        return;
+      console.log("Fim da página");
     }
-    
-    window.addEventListener('wheel', infiniteScroll)
-  })
+
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
+    return () => {
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
+    };
+  }, []);
 
   return (
     <div>
       {modalPhoto && (
-        <FeedModal user={user} photo={modalPhoto} setModalPhoto={setModalPhoto} />
+        <FeedModal
+          user={user}
+          photo={modalPhoto}
+          setModalPhoto={setModalPhoto}
+        />
       )}
       <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
