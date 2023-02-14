163a3236608ff9f1b8612e28cec2531991e7193a
cyd losekann <cydnel@gmail.com>
2023-02-14 16:21:41 -0300

    photo comments css

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,41 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./PhotoContent.module.css";
-import PhotoComments from "./PhotoComments";
-import PhotoDelete from "./PhotoDelete";
-import { UserContext } from "../../UserContext";
-import Image from "../Helper/Image";
+import React from 'react';
+import styles from './PhotoContent.module.css';
+import { Link } from 'react-router-dom';
+import PhotoComments from './PhotoComments';
+import { UserContext } from '../../UserContext';
+import PhotoDelete from './PhotoDelete';
+import Image from '../Helper/Image';
 
 const PhotoContent = ({ data }) => {
-  const { photo, comments } = data;
   const user = React.useContext(UserContext);
+  const { photo, comments } = data;
 
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
         <Image src={photo.src} alt={photo.title} />
-        <div className={styles.details}>
-          <div>
-            <p className={styles.author}>
-              {user.data && user.data.username === photo.author ? (
-                <PhotoDelete id={photo.id} />
-              ) : (
-                <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-              )}
-              <span className={styles.visualizacoes}>{photo.acessos}</span>
-            </p>
-            <h1 className="title">
-              <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
-            </h1>
-            <ul className={styles.attributes}>
-              <li>{photo.peso} kg</li>
-              <li>
-                {photo.idade === "1"
-                  ? `${photo.idade} ano`
-                  : `${photo.idade} anos`}
-              </li>
-            </ul>
-          </div>
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
         </div>
       </div>
       <PhotoComments id={photo.id} comments={comments} />
