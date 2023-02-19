a7924fcfac7001942f1d5ec441790c12207ed509
cyd losekann <cydnel@gmail.com>
2023-02-19 01:22:56 -0300

    single

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -21,7 +21,7 @@ const PhotoComments = (props) => {
           </li>
         ))}
       </ul>
-      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+      {login && <PhotoCommentsForm single={props.single} id={props.id} setComments={setComments} />}
     </>
   );
 };
