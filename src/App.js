a0454a4dc66a75cc8de1bb0e2c0f2a55214eb5ae
cyd losekann <cydnel@gmail.com>
2023-02-21 09:06:04 -0300

    /conta?

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Home from "./Components/Home";
-import Login from "./Components/Login/Login";
-import User from "./User/User";
-import { UserStorage } from "./UserContext";
-import Photo from "./Components/Photo/Photo";
-import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
 
 function App() {
   return (
@@ -19,17 +19,15 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
-            <ProtectedRoute path="conta/*" element={<User />} />
-            <Route path="/foto/:id" element={<Photo />} />
             <Route
               path="conta/*"
               element={
                 <ProtectedRoute>
-                  {" "}
                   <User />
                 </ProtectedRoute>
               }
             />
+            <Route path="foto/:id" element={<Photo />} />
           </Routes>
           <Footer />
         </UserStorage>
