1c701cd904dc8dbc301a4b1025f4d43b739e59ec
cyd losekann <cydnel@gmail.com>
2023-02-23 13:38:30 -0300

    Lost password?

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginPasswordLost = () => {
+  const email = useForm();
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log(event);
   }
-  
+
   return (
     <section>
       <h1 className="title">Perdeu a senha?</h1>
       <form onSubmit={handleSubmit}>
-        <Input />
-        <Button> Enviar e-mail </ Button>
+        <Input label="Email / Usuário" type="text" name="email" {...email} />
+        <Button> Enviar email </ Button>
       </form>
     </section>
   );
