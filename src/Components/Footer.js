785a3f13538ddb2a2cbb95a7b71e4bff0d5f5d2c
cyd losekann <cydnel@gmail.com>
2023-02-28 12:57:05 -0300

    correcoes finais

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import styles from './Header.module.css'
+import styles from './Footer.module.css'
 import { ReactComponent as Dogs } from '../Assets/dogs.svg'
 
 const Footer = () => {
-  return <footer className={styles.footer}>
+  return  (
+  <footer className={styles.footer}>
     <Dogs />
     <p> Dogs. Alguns direitos reservados. </p>
   </footer>
+  )
 };
 
 export default Footer
