894229d1662a1ed093255f7d451898948cc02965
cyd losekann <cydnel@gmail.com>
2023-03-01 04:12:07 -0300

    Head

--- src/Components/Helper/Head.js
+++ src/Components/Helper/Head.js
@@ -1,18 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const Head = (props) => {
   React.useEffect(() => {
-    document.title = props.title + " | Dogs" ;
+    document.title = props.title + ' | Dogs';
     document
-      .querySelector('meta[name="description"]')
-      .setAttribute("content", props.description || "");
+      .querySelector("meta[name='description']")
+      .setAttribute('content', props.description || '');
   }, [props]);
-  return (
-    <div className="head">
-      <title> Título da página </title>
-      <meta name="description" content="Descrição da página" />
-    </div>
-  );
+
+  return <></>;
 };
 
 export default Head;
